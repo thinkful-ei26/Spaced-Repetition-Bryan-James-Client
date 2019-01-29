74e01465a822874d51d54495f668fd6a6ba2c3f6
James Bell <Noxachron@gmail.com>
2019-01-29 10:10:02 -0500

    heroku

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -9,7 +9,7 @@ export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN'
 export const setAuthToken = authToken => ({
   type: SET_AUTH_TOKEN,
   authToken,
-})
+});
 
 export const CLEAR_AUTH = 'CLEAR_AUTH'
 export const clearAuth = () => ({

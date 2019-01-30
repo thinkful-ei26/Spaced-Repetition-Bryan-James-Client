b5da4ff2498cd0067d64790af6c3b2d890b60251
James Bell <Noxachron@gmail.com>
2019-01-30 10:47:12 -0500

    changed questions score to session score

--- src/components/progress.js
+++ src/components/progress.js
@@ -4,14 +4,14 @@ import requiresLogin from './requires-login'
 
 export class Progress extends Component {
   render() {
-    let currentQuestion = this.props.all.filter(eachQ=> eachQ.id===this.props.current)
-    let currentQuestionScore = 0;
-    if(currentQuestion.length > 0){
-      currentQuestionScore = currentQuestion[0].count;
-    }
+    //let currentQuestion = this.props.all.filter(eachQ=> eachQ.id===this.props.current)
+    //let currentQuestionScore = 0;
+    // if(currentQuestion.length > 0){
+    //   currentQuestionScore = currentQuestion[0].count;
+    // }
     return (
       <div>
-        <p className='score'>This question Score: {currentQuestionScore} / 10</p>
+        <p className='score'>Session Score: {this.props.score} / 10</p>
       </div>
     )
   }
@@ -22,7 +22,8 @@ const mapStateToProps = state => {
   return ({
     validateData: state.validateResponse.data,
     current: state.protectedData.data.id,
-    all: state.protectedData.allQuestions
+    all: state.protectedData.allQuestions,
+    score: state.protectedData.sessionScore
   })
 }
 
--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -9,6 +9,7 @@ import {
 const initialState = {
     data: '',
     allQuestions : [],
+    sessionScore : 0,
     error: null
 };
 
@@ -24,7 +25,7 @@ export default function reducer(state = initialState, action) {
         });
     } else if(action.type === FETCH_ALL_QUESTIONS_SUCCESS){
         let countified = action.data.map(item=> {
-            item.count = 0;
+            item.correct = false;
             return item;
         });
         
@@ -35,19 +36,31 @@ export default function reducer(state = initialState, action) {
     } else if(action.type === COUNT_CORRECT){
         let newCount = state.allQuestions.map(item=> {
             if(item.id===action.data.id){
-                item.count++;
+                item.correct = true;
             }
             return item;
         });
-        return Object.assign({}, state, {allQuestions: [...newCount]});
-    } else if(action.type === COUNT_WRONG){
-        let newCount = state.allQuestions.map(item=> {
-            if(item.id===action.data.id){
-                (item.count >0)? item.count--: item.count = 0;
-            }
-            return item;
-        });
-        return Object.assign({}, state, {allQuestions: [...newCount]});
+        let newScore = state.sessionScore + 1;
+        return Object.assign({}, state, {allQuestions: [...newCount], sessionScore: newScore});
+    }
+     else if(action.type === COUNT_WRONG){
+        // let newCount = state.allQuestions.map(item=> {
+        //     if(item.id===action.data.id){
+        //         (item.count >0)? item.count--: item.count = 0;
+        //     }
+        //     return item;
+        // });
+        let wrongBefore = state.allQuestions.filter(item=>item.id===action.data.id);
+        let newDecrement = 0;
+        if (wrongBefore[0].correct){
+            newDecrement = 1;
+        }
+        let newScore = state.sessionScore - newDecrement;
+        console.log("score:," , newScore)
+        if(newScore < 0){
+            newScore = 0;
+        }
+        return Object.assign({}, state, {sessionScore: newScore});
     }
     return state;
 }
